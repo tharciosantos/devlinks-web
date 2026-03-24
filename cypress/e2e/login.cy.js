734441ba7354068c2752a765aafebbb36a3ee7f7
Tharcio Santos <tharcio09@hotmail.com>
2026-03-24 02:34:32 -0300

    test(e2e): configura cypress e adiciona teste de fluxo de login
    Implementa teste ponta a ponta (E2E) para validar o preenchimento do formulário, recebimento do token e redirecionamento para rota protegida.

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('Fluxo de Autenticação (Login)', () => {
+  it('Deve fazer login com sucesso e ir para o Dashboard', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('input[type="email"]').type('th222@gmail.com');
+    cy.get('input[type="password"]').type('12345');
+    cy.get('button[type="submit"]').click('');
+    cy.url().should('include', '/dashboard');
+    cy.contains('Usuários do Sistema:').should('be.visible');
+  });
+})
