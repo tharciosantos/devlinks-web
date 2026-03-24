f2391431b115770e58c5fd23bafc11ba704161ff
Tharcio Santos <tharcio09@hotmail.com>
2026-03-24 02:41:16 -0300

    chore: adicionar suporte do Cypress no ESLint (flat config)

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import cypress from 'eslint-plugin-cypress'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
+  // 🔹 Seu config atual
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -24,6 +27,23 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+
+  // BLOCO PARA CYPRESS
+  {
+    files: ['cypress/**/*.js'],
+    plugins: {
+      cypress,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...cypress.environments.globals.globals,
+      },
+    },
+    rules: {
+      ...cypress.configs.recommended.rules,
     },
   },
 ])
